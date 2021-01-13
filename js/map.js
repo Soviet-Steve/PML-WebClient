3ac0248e7d852e7433f14014e25f830b2d7e5410
Benjamin Stanwell <benjaminstanwell@gmail.com>
2021-01-13 16:22:22 +1100

    Web Map interface finished

---
+++ js/map.js
@@ -0,0 +1,136 @@
+// Setting a higher initial zoom to make effect more obvious
+const mymap = L.map('PMLmap').setView([-32.55, 151.45], 6);
+// const tileUrl = './assests/maptiles/{z}/{x}/{y}.png';
+// const tileUrl = './assests/newcastleMaping/{z}/{x}/{y}.png';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl/*, {maxZoom: 14}*/);
+tiles.addTo(mymap);
+
+// Making a marker with a custom icon
+const cowIcon = L.icon({
+    iconUrl: 'assests/cow.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 20]
+});
+
+const farmerIcon = L.icon({
+    iconUrl: 'assests/farmer.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 20]
+});
+
+const bikerIcon = L.icon({
+    iconUrl: 'assests/biker.svg',
+    iconSize: [40, 40],
+    iconAnchor: [20, 20]
+});
+
+function iconChooser(input){
+    switch(input){
+        case "Cow Tag":
+            return cowIcon;
+        case "Farmer Tag":
+            return farmerIcon;
+        case "Bike Tag":
+            return bikerIcon;
+        default:
+            return null;
+    }
+}
+
+
+
+var comms = [ // 0 ref, 1 ID, 2 has message, 3 lat, 4 lng, 5 time, 6 message
+    [2, 2, false, -32.84, 151.705, "2021-01-13 00:50:57", null],
+    [3, 6, false, -32.8924, 151.704, "2021-01-13 00:50:57", null]
+]
+
+var info = [ // 0 ID, 1 Name, 2 in use, 3 type
+    [2, "Daisy", true, "Cow Tag"],
+    [6, "Andrew", true, "Bike Tag"]
+]
+
+
+
+// var xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function(){
+//     if(this.readyState == 4 && this.status == 200){
+//         var myObj = JSON.parse(this.responseText);
+//         for(var i = 0; i < myObj.length; i++){
+//             switch(myObj[i].length){
+//                 case 4:
+//                     info[info.length] = myObj[i];
+//                     break;
+//                 case 7:
+//                     comms[comms.length] = myObj[i];
+//                     break;
+//                 default:
+//             }
+//         }
+//     }
+//     console.log(info);
+// }
+
+// xhr.open("GET", "./php/fetchDatabase.php", true);
+// xhr.send();
+
+let firstTime = true;
+async function fetchData() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', './php/fetchDatabase.php', true)
+    xhr.onload = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var myObj = JSON.parse(this.responseText);
+            info = [];
+            comms = [];
+            for(var i = 0; i < myObj.length; i++){
+                switch(myObj[i].length){
+                    case 4:
+                        info[info.length] = myObj[i];
+                        break;
+                    case 7:
+                        comms[comms.length] = myObj[i];
+                        break;
+                    default:
+                }
+            }
+            var activeTags = [];
+            for(var i = 0; i < info.length; i++){
+                if(info[i][2] == 1){
+                    activeTags.push(info[i]);
+                }
+            }
+
+
+
+            var items = []; // 0 Name, 1 ID, 2 Lat, 3 Lng, 4 time, 5 type
+
+            for(var i = 0; i < activeTags.length; i++){
+                items.push([activeTags[i][1], activeTags[i][0], /*lat*/, /*lng*/, /*time*/ , activeTags[i][3]]);
+            }
+
+
+
+            for(var i = 0; i < comms.length; i++){
+                for(var j = 0; j < items.length; j++){
+                    if(comms[i][1] == items[j][1]){
+                        items[j][2] = comms[i][3];
+                        items[j][3] = comms[i][4];
+                        items[j][4] = comms[i][5];
+                    }
+                }
+            }
+
+            for(var i = 0; i < items.length; i++){
+                marker = new L.marker([items[i][2],items[i][3]], { icon: iconChooser(items[i][5])})
+                            .bindPopup(items[i][0] + "<br>Lat: " + items[i][2] +"<br>Lng: " + items[i][3] + "<br>Last updated: " +items[i][4] + "<br>Tag ID: " +items[i][1])
+                            .addTo(mymap);
+            }
+            console.log(info);
+        }
+    }
+    xhr.send();
+}
+fetchData();
+setInterval(fetchData, 10000);
+
--- php/fetchDatabase.php
+++ php/fetchDatabase.php
@@ -1,3 +1,39 @@
 <?php
+require_once 'fetchDatabase.php';
+$hn = 'localhost';
+$db = 'pml_tag';
+$un = 'tag_user';
+$pw = 'cowdatabasepassword';
+$conn = new mysqli($hn, $un, $pw, $db);
+if($conn->connect_error) die("Fatal error");
 
-$conn = mysqli_connect("localhost", "root", )
+$info = "SELECT * FROM Info";
+$info = $conn->query($info);
+
+$comms = "SELECT * FROM Comms";
+$comms = $conn->query($comms);
+
+
+
+$info_output = array($info->fetch_array(MYSQLI_NUM));
+
+for($i = 0; $i < $info->num_rows - 1; $i++){
+    $temp = array($info->fetch_array(MYSQLI_NUM));
+    $info_output = array_merge($info_output, $temp);
+}
+
+$comms_output = array($comms->fetch_array(MYSQLI_NUM));
+
+for($i = 0; $i < $comms->num_rows - 1; $i++){
+    $temp = array($comms->fetch_array(MYSQLI_NUM));
+    $comms_output = array_merge($comms_output, $temp);
+}
+
+$output = array_merge($comms_output, $info_output);
+
+echo json_encode($output);
+
+
+
+mysqli_close($conn);
+?>
